79b52fb109994d8e6f36c247b5fa581b41299624
Ernest <ekedesh@gmail.com>
2021-09-21 20:46:55 +0300

    [R]: auth router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from .auth import router as auth_router
-
+from .propositions import router as proposition_router
 
 router = APIRouter()
 router.include_router(auth_router)
+router.include_router(proposition_router)
